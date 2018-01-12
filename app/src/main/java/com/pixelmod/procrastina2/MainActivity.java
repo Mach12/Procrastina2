87f985152503644cff0de09b29adaf562ffe023c
Antonin Charrier <acharrier@intechinfo.fr>
2018-01-12 15:22:01 +0100

    whaaaaatever

--- app/src/main/java/com/pixelmod/procrastina2/MainActivity.java
+++ app/src/main/java/com/pixelmod/procrastina2/MainActivity.java
@@ -2,6 +2,14 @@ package com.pixelmod.procrastina2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
